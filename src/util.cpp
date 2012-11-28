26bada3cb1ccbbcb1a437f3f4e2f942e90102e6d
Yuki Takahashi <takahahsi9854@gmail.com>
2012-11-28 11:21:37 +0900

    modified xeuclid in utild.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -10,3 +10,16 @@ int gcd(int a,int b){
 
   return a;
 }
+
+void xeuclid(const int a,const int b,int *u,int *v,int *g){
+  int x=0,y=b; *u=1,*g=a;
+  int s,t,q;
+  while(y!=0){
+    t=*g%y;
+    q=(*g-t)/y;
+    s=*u - q * x;
+    *u = x; *g = y;
+    x = s; y = t;
+  }
+  *v = (*g - a * (*u))/b;
+}
