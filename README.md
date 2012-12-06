666395ff0f68884bb8a557345a5b5244d3d07e6a
Yuki Takahashi <takahashi9854@gmail.com>
2012-12-06 09:01:45 +0900

    modified main.cpp and add instruction in README.md

--- README.md
+++ README.md
@@ -4,3 +4,48 @@ BGstep
 Baby step, Giant Step.
 Since Nov 26th.
 
+=====
+
+This followings are the  algorithm of Baby step, Giant step.
+  
+Let #E(F_q) = N.
+1.Compute Q = (q+1)P.
+
+2.Choose an integer m with more than 4-th root of q. Compute and store
+the Points jP for j=0,1,2,...,m.
+
+3.Compute the points
+Q + k(2m)P for k=-m,-(m-1),...,m
+until there is a match Q+k(2m)P = \pm jP with a point (or its negative) on the stored list.
+
+4.Conclude that (q+1+2mk \mp j)P = \infty. Let M = q+1+2mk \mp j.
+	   
+5.Factor M. Let p_1,....,p_r be the distinct prime factors of M.
+
+6.Compute (M/p_i) for i=1,...,r. If (M/p_i)P = \infty for some i, replace M with M/p_i and go back to step (5).
+If (M/p_i)P \neq \infty for all i then M is the order of the point P.
+
+7.If we are looking for the #E(P_q), then repeat step (1)-(6) with randomly chosen points in E(F_q) until
+the greatest common multiple of the orders divides only one integer N between q+1-2\sqrt{q} and q+1+\sqrt{q}.
+Then N = #E(F_q).
+
+====
+
+Remarks follows.
+  
+To save storage space, it might be more efficient to store only the x coordinates of the points jP
+(along with the corresponding integer j), since looking for a match with \pm jP only require the x-coordinate
+(assuming we are working with a Weierstrass equation). When a match is found, the two possible y-coordinates
+can be recomputed.
+
+Computing Q+k(2m)P can be done by computing Q and 2mP once for all. To get from Q+k(2m)P to Q+(k+1)(2mP),
+simply add 2mP rather than recomputing everything. Similarly, once jP has been computed, add P to get (j+1)P.
+
+We are assuming that we can factor M. If not, we can at least find all the small prime
+factor p_i and check that (M/p_i)P\neq \infty for these. Then M will be good candidate for the order of P.
+
+Why is the method called "Baby Step, Giant Step"? The baby steps are from a point jP to (j+1)P.
+The giant steps are from a point k(2m)P to (k+1)(2m)P, since we take the "bigger" step 2mP.
+
+ 
+
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cmath>
 
+
 int main(int argc, char *argvp[]){
 
   // y^2 = x^3 + ax^2 + bx + c
@@ -14,27 +15,58 @@ int main(int argc, char *argvp[]){
   Point::c=2;
 
   int i,j,k;
-  
-  long long q = 7; // F_q
-  long long m = sqrt(sqrt(q));
-  Point *store = new Point[m+1]; // default constructor.
 
-  Point P(3,1,1);
-  Point R(3,2,1);
-  Point rP;
-  Point Q = P.ECpower(q+1,q);
-  Point result,tmp,rtmp;
-  for(i=0;i<=m;i++) store[i] = P.ECpower(i,q);
-  k = m;
-  while(k>=0){
-    tmp.ECadd(Q,P.ECpower(k*2*m,q));
-    rmpt = tmp.reflect();
-    if(tmp == store[m]) break;
-    if(rmpt == store[m]) break;
+  long long M;
+  long long q = 7; // F_q
+  long long m = floor(sqrt(sqrt(q))) + 1; // calculate the forth root of q.
+
+  // here is the step 1.
+  Point P(3,1,1); // want to find the order of this point.
+  Point Q = P.ECpower(q+1,q); // calculate Q = (q+1)P.
+
+  // here is the step 2.
+  Point *store = new Point[m+1]; // default constructor.
+  for(i=0;i<=m;i++) store[i] = P.ECpower(i,q); // store jP for j=0,1,2,...,m.
+
+  Point _2mP = P.ECpower(2*m,q);
+  Point R; // store Q + k(2m)p. that mean, Q + k*(_2mP);
+  Point result;
+
+  // show _2mP.
+  std::cout << "coordinates of _2mP." << std::endl;b
+  std::cout << "(" << _2mP.x << "," << _2mP.y << "," << _2mP.z << ")" << std::endl;
+ 
+  // show all points in store[]
+  for(int i=0;i<=m;i++)
+    std::cout << "(" << store[i].x << "," << store[i].y << "," << store[i].z << ")" << std::endl;
+
+  // start checking points are equall to R.
+  // here is the step 3.
+  k = 0;
+  R.ECadd(Q,_2mP.ECpower(k,q),q);
+  std::cout << "R.x = " << R.x << std::endl;
+  while(k<=m){
+    std::cout << "store[" << k << "] = " << store[k].x << std::endl;
+    if(store[k].x == R.x){
+      break;
+    }
+    k++;
   }
-      
-    
+  result = store[k];
+  if(result == R){
+    M = q + 1 + 2*m*k + 1;
+  }else{
+    M = q + 1 + 2*m*k - 1;
+  }
+  std::cout << "k = " << k << std::endl;
+  std::cout << "m = " << m << std::endl;
+  std::cout << "q = " << q << std::endl;
+  std::cout << M << std::endl;
   
-  delete [] store;
+    
+
+    
+  delete [] store; // destoroy array.
+
   return 0;
 }
